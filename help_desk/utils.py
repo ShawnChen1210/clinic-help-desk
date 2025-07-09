dec68dddace353aa759d875fd913fcc20a41e957
Shawn Chen <48112492+ShawnChen1210@users.noreply.github.com>
2025-07-08 22:41:37 -0700

    added google drive build resource, added UserInfo model, and registration html

--- help_desk/utils.py
+++ help_desk/utils.py
@@ -7,8 +7,15 @@ from django.conf import settings
 def get_google_sheets_service_creds():
     creds = service_account.Credentials.from_service_account_file(
         settings.GOOGLE_SERVICE_ACCOUNT_KEY,
-        #allows for editing both sheets and drive files
+        #some google sheets operation require google drive scope permissions, thus its inclusion
         scopes=['https://www.googleapis.com/auth/spreadsheets',
                 'https://www.googleapis.com/auth/drive']
     )
     return build('sheets', 'v4', credentials=creds)
+
+def get_google_drive_service_creds():
+    creds = service_account.Credentials.from_service_account_file(
+        settings.GOOGLE_SERVICE_ACCOUNT_KEY,
+        scopes=['https://www.googleapis.com/auth/drive']
+    )
+    return build('drive', 'v3', credentials=creds)
--- help_desk/views.py
+++ help_desk/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils import *
 # Create your views here.
 
 def index(request):
