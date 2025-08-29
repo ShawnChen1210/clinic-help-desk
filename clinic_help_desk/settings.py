1428d3cc48b245cf2d9e8f856659bbd40c87b992
Shawn Chen <48112492+ShawnChen1210@users.noreply.github.com>
2025-08-29 02:09:19 -0700

    added better loggin

--- clinic_help_desk/settings.py
+++ clinic_help_desk/settings.py
@@ -195,3 +195,35 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 LOGIN_URL = '/registration/login_user/'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",  # Change to "DEBUG" for more verbosity
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+}
