ecf1548494202056c8dff211ffadec06c97e7809
Shawn Chen <48112492+ShawnChen1210@users.noreply.github.com>
2025-08-28 17:36:09 -0700

    updated debug in settings to use env variable

--- clinic_help_desk/settings.py
+++ clinic_help_desk/settings.py
@@ -29,7 +29,7 @@ SHARED_DRIVE_ID = '0AItIf3a1ARFYUk9PVA'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
