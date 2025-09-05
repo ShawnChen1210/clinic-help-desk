ec4e9f0a6b590ca774dc0a1f816c2d227d9f6a18
Shawn Chen <48112492+ShawnChen1210@users.noreply.github.com>
2025-09-05 17:09:30 -0400

    made tailwind config look for more than just textscript files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: [
       "./src/**/*.{js,jsx,ts,tsx}",
+      "./templates/**/*.html",           // Add this for global templates
+      "./home/templates/**/*.html",      // Add this for your home app templates
+      "./**/templates/**/*.html",        // Add this to catch all app templates
+      "./static/js/**/*.js",
   ],
   theme: {
     extend: {},
